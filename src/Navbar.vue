41327acfc9319df5125d05655125139716da3a20
Nathaniel Johnson <nathancj224@gmail.com>
2018-12-07 13:44:51 -0500

    added navbar

---
+++ src/Navbar.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+
+  }
+</script>
+<template>
+  <div id="nav">
+    <a href="/" class="logo">LazzySounds</a>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  font-size: 25px;
+}
+ul li {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
